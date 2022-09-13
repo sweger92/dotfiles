efc7cd94d517ed1a899c21b88fea0a5e6d5c9c58
Seth Weger <seth.weger@waitrapp.com>
2022-09-12 20:21:45 -0500

    Add which-key nvim plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -100,6 +100,18 @@ return packer.startup(function(use)
     -- python repl
     use {'hkupty/iron.nvim'}
 
+    -- which-key
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+
 	-- use 'itchyny/lightline.vim'
 
 	-- -- file explorer
